3e624470e9cd235f6803a7f37cf78bffbd860fa6
Anthony Casagrande <acasagrande@nvidia.com>
2025-05-16 14:36:00 -0700

    add back exceptions

---
+++ aiperf/common/exceptions/service_exceptions.py
@@ -0,0 +1,64 @@
+#  SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#  SPDX-License-Identifier: Apache-2.0
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from aiperf.common.exceptions.base_exceptions import AIPerfException
+
+
+class ServiceException(AIPerfException):
+    """Base class for all exceptions raised by services."""
+
+    pass
+
+
+class ServiceInitializationException(ServiceException):
+    """Exception raised for service initialization errors."""
+
+    pass
+
+
+class ServiceStartException(ServiceException):
+    """Exception raised for service start errors."""
+
+    pass
+
+
+class ServiceStopException(ServiceException):
+    """Exception raised for service stop errors."""
+
+    pass
+
+
+class ServiceCleanupException(ServiceException):
+    """Exception raised for service cleanup errors."""
+
+    pass
+
+
+class ServiceMessageProcessingException(ServiceException):
+    """Exception raised for service message processing errors."""
+
+    pass
+
+
+class ServiceRegistrationException(ServiceException):
+    """Exception raised for service registration errors."""
+
+    pass
+
+
+class ServiceStatusException(ServiceException):
+    """Exception raised for service status errors."""
+
+    pass
