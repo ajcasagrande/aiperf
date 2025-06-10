04c5012d008b5fafea9804d41adc55eda6f3dd7e
Elias Bermudez <dbermudez@nvidia.com>
2025-06-10 11:14:51 -0700

    Update get records call

--- aiperf/tests/test_metric_summary.py
+++ aiperf/tests/test_metric_summary.py
@@ -14,7 +14,7 @@ def test_metric_summary_process_and_get_metrics():
     resp2 = Transaction(timestamp=20, payload="payload2")
     records.add_record(request=req, responses=[resp1, resp2])
 
-    ms.process(records.get_records())
+    ms.process(records.records)
 
     for tag, metric in ms.get_metrics().items():
         assert tag == "ttft"
