67802919b9c55bcb49b4416016ab9e83cae3c39e
Elias Bermudez <dbermudez@nvidia.com>
2025-06-09 17:08:03 -0700

    Add initial metric support

---
+++ aiperf/services/records_manager/metrics/metric.py
@@ -0,0 +1,88 @@
+#  SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#  SPDX-License-Identifier: Apache-2.0
+import importlib
+import os
+from abc import ABCMeta, abstractmethod
+
+from aiperf.common.exceptions import MetricTypeError
+from aiperf.services.records_manager.records import Record
+
+
+class MetricInterface(ABCMeta):
+    metric_interfaces: dict[str, "MetricInterface"] = {}
+
+    def __new__(cls, name, base, namespace):
+        """
+        This function is called upon declaration of any classes of type
+        MetricInferface. It registers the class in the metric_interface
+        dictionary using the `tag` attribute of the class as the key.
+        The `tag` attribute should be a string that uniquely identifies the
+        metric type. If the `tag` is not a string, it will not be registered.
+
+        """
+
+        metric_interface = super().__new__(cls, name, base, namespace)
+
+        if isinstance(metric_interface.tag, str):
+            cls.metric_interfaces[metric_interface.tag] = metric_interface
+        return metric_interface
+
+    @classmethod
+    def get(cls, tag: str) -> "MetricInterface":
+        # TODO: Need to update with importlib logic
+        return cls.metric_interfaces[tag]
+
+    @classmethod
+    def get_all(
+        cls,
+    ) -> dict[str, "MetricInterface"]:
+        """
+        Returns the dictionary of all registered metric interfaces.
+        """
+        type_module_directory = os.path.join(
+            globals()["__spec__"].origin.rsplit("/", 1)[0], "types"
+        )
+        for filename in os.listdir(type_module_directory):
+            if filename != "__init__.py" and filename.endswith(".py"):
+                try:
+                    importlib.import_module(
+                        f"aiperf.services.records_manager.metrics.types.{filename[:-3]}"
+                    )
+                except AttributeError as err:
+                    raise MetricTypeError("Error retrieving all metric types") from err
+
+        return cls.metric_interfaces
+
+
+class Metric(metaclass=MetricInterface):
+    @property
+    @abstractmethod
+    def tag(self) -> str:
+        """
+        Returns
+        -------
+        str
+            the name tag of the record type.
+        """
+
+    @abstractmethod
+    def add_record(self, record: Record) -> None:
+        """
+        Adds a new record and calculates the new metric value.
+        """
+
+    @abstractmethod
+    def get_metrics(self) -> list[int]:
+        """
+        Returns the list of calculated metrics.
+        """
+
+    @staticmethod
+    @abstractmethod
+    def _check_record(record: Record) -> None:
+        """
+        Checks if the record is valid for metric calculation.
+
+        Raises:
+            ValueError: If the record does not meet the required conditions.
+        """
--- aiperf/services/records_manager/post_processors/metric_summary.py
+++ aiperf/services/records_manager/post_processors/metric_summary.py
@@ -4,10 +4,13 @@
 import logging
 
 from aiperf.common.enums import PostProcessorType
-from aiperf.common.factories import PostProcessFactory
+from aiperf.common.factories import PostProcessorFactory
+from aiperf.services.records_manager.metrics.metric import MetricInterface
+
+logger = logging.getLogger(__name__)
 
 
-@PostProcessFactory.register(PostProcessorType.METRIC_SUMMARY)
+@PostProcessorFactory.register(PostProcessorType.METRIC_SUMMARY)
 class MetricSummary:
     """
     MetricSummary is a post-processor that generates a summary of metrics from the records.
@@ -18,11 +21,24 @@ class MetricSummary:
         self.logger = logging.getLogger(__name__)
         self.logger.debug("Initializing MetricSummary post-processor")
 
-    def process(self, records: list) -> dict:
+        self._metrics = []
+        for metric_cls in MetricInterface.get_all().values():
+            self._metrics.append(metric_cls())
+
+    def process(self, records: list) -> None:
         """
         Process the records to generate a summary of metrics.
 
         :param records: The input records to be processed.
         :return: A dictionary containing the summarized metrics.
         """
-        self.logger.debug("Processing records for metric summary")
+        for record in records:
+            for metric in self._metrics:
+                metric.add_record(record)
+
+    def get_metrics(self) -> dict:
+        metrics_summary = {}
+        for metric in self._metrics:
+            metrics_summary[metric.tag] = metric.get_metrics()
+
+        return metrics_summary
