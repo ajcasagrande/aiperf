bd9ae6ae85758e7d5e5bcf1cd2fb6c86cd5553a3
Anthony Casagrande <acasagrande@nvidia.com>
2025-06-14 03:33:20 -0700

    mostly working dask

--- aiperf/services/worker_manager/__init__.py
+++ aiperf/services/worker_manager/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-__all__ = ["WorkerManager"]
+__all__ = ["WorkerManager", "DaskWorkerManager"]
 
+from aiperf.services.worker_manager.dask_worker_manager import DaskWorkerManager
 from aiperf.services.worker_manager.worker_manager import WorkerManager
