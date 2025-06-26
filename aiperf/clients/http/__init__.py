197f6f43de348dbe429277e06715cdf36512261c
Anthony <acasagrande@nvidia.com>
2025-06-26 10:17:35 -0700

    fix issues after merge and with tests

--- aiperf/clients/http/__init__.py
+++ aiperf/clients/http/__init__.py
@@ -4,8 +4,8 @@ from aiperf.clients.http.aiohttp_client import (
     AioHttpClientMixin,
     AioHttpSSEStreamReader,
     create_tcp_connector,
+    parse_sse_message,
 )
-from aiperf.clients.http.sse_utils import parse_sse_message
 
 __all__ = [
     "AioHttpClientMixin",
--- aiperf/tests/clients/http/test_sse_utils.py
+++ aiperf/tests/clients/http/test_sse_utils.py
@@ -4,7 +4,7 @@ import time
 
 import pytest
 
-from aiperf.clients.http.sse_utils import parse_sse_message
+from aiperf.clients.http import parse_sse_message
 from aiperf.common.enums import SSEFieldType
 from aiperf.common.models import SSEField, SSEMessage
 
