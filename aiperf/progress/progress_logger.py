418ce968010aa8f74ec2f69e8bbcd93aeb562206
Anthony Casagrande <acasagrande@nvidia.com>
2025-07-29 17:56:08 -0700

    latest

--- aiperf/progress/progress_logger.py
+++ aiperf/progress/progress_logger.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from rich import console
+
 from aiperf.common.constants import NANOS_PER_MILLIS
 from aiperf.common.enums import CreditPhase, MessageType, MetricTag
 from aiperf.common.hooks import background_task, on_message
@@ -23,6 +25,7 @@ class ProgressLogger(MessageBusClientMixin):
             type=CreditPhase.PROFILING
         )
         self.metrics_preview: ProfileResults | None = None
+        self.console = console.Console()
 
     @on_message(MessageType.CREDIT_PHASE_START)
     async def _on_credit_phase_start(self, message: CreditPhaseStartMessage) -> None:
@@ -90,33 +93,34 @@ class ProgressLogger(MessageBusClientMixin):
             }
             for stat_type in stat_types
         }
-        results: list[str] = []
-        delimiter = " / "
 
         formatters = {
-            MetricTag.TTFT: (lambda x: f"{x / NANOS_PER_MILLIS:.2f}", "ms"),
-            MetricTag.TTST: (lambda x: f"{x / NANOS_PER_MILLIS:.2f}", "ms"),
-            MetricTag.INTER_TOKEN_LATENCY: (
-                lambda x: f"{x / NANOS_PER_MILLIS:.2f}",
-                "ms",
-            ),
-            MetricTag.REQUEST_LATENCY: (lambda x: f"{x / NANOS_PER_MILLIS:.2f}", "ms"),
-            MetricTag.ISL: (lambda x: f"{x:.1f}", "tokens"),
-            MetricTag.OSL: (lambda x: f"{x:.1f}", "tokens"),
-            MetricTag.OUTPUT_TOKEN_THROUGHPUT: (lambda x: f"{x:,.1f}", "tokens/s"),
+            MetricTag.TTFT: lambda x: f"{x / NANOS_PER_MILLIS:.2f}",
+            MetricTag.TTST: lambda x: f"{x / NANOS_PER_MILLIS:.2f}",
+            MetricTag.INTER_TOKEN_LATENCY: lambda x: f"{x / NANOS_PER_MILLIS:.2f}",
+            MetricTag.REQUEST_LATENCY: lambda x: f"{x / NANOS_PER_MILLIS:.2f}",
+            MetricTag.ISL: lambda x: f"{x:.1f}",
+            MetricTag.OSL: lambda x: f"{x:.1f}",
+            MetricTag.OUTPUT_TOKEN_THROUGHPUT: lambda x: f"{x:.1f}",
         }
 
-        for metric_tag in formatters:
-            if metric_tag in metrics[stat_types[0]]:
-                vals = [
-                    formatters[metric_tag][0](metrics[stat_type][metric_tag])
+        res = {
+            "metrics": {
+                metric_tag: {
+                    stat_type: float(
+                        formatters[metric_tag](metrics[stat_type][metric_tag])
+                    )
                     for stat_type in stat_types
-                ]
-                results.append(
-                    f"{metric_tag}: {delimiter.join(vals)} {formatters[metric_tag][1]}"
-                )
-
-        newline = "\n"
-        self.info(
-            f"Metrics Preview {stat_types}: {newline.join(results)} ({self.metrics_preview.completed:,} records)"
-        )
+                }
+                for metric_tag in formatters
+            },
+            "progress": {
+                "completed": self.metrics_preview.completed,
+                "total_expected": self.metrics_preview.total_expected,
+                "progress": f"{self.metrics_preview.completed / self.metrics_preview.total_expected * 100:.1f}%"
+                if self.metrics_preview.total_expected
+                else "N/A",
+            },
+        }
+        self.console.print(res)
+        return
