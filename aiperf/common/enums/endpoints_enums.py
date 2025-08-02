4944f00753bae3785fdfe2b758300fa56fb9cc34
Anthony Casagrande <acasagrande@nvidia.com>
2025-08-02 16:52:13 -0700

    add tests for enums

--- aiperf/common/enums/endpoints_enums.py
+++ aiperf/common/enums/endpoints_enums.py
@@ -8,9 +8,11 @@ from aiperf.common.enums.base_enums import CaseInsensitiveStrEnum
 
 
 class EndpointInfo(BaseModel):
-    """Pydantic model for endpoint-specific information."""
+    """Pydantic model for endpoint-specific metadata. This model us used to store additional info on each EndpointType enum value."""
 
-    tag: str = Field(..., description="The string value for the endpoint type.")
+    tag: str = Field(
+        ..., min_length=1, description="The string value for the endpoint type."
+    )
     supports_streaming: bool = Field(
         ..., description="True if the endpoint supports streaming, False otherwise."
     )
@@ -23,16 +25,17 @@ class EndpointInfo(BaseModel):
     )
     metrics_title: str | None = Field(
         None,
-        description="The title string for the endpoint type. If None, the endpoint does not have a specific title.",
+        description="The title string for the metrics table. If None, the default title is used.",
     )
 
 
 class EndpointType(CaseInsensitiveStrEnum):
-    """Endpoint types.
+    """Endpoint types supported by AIPerf.
 
-    These determine the format of request payload to send to the model.
-
-    Similar to `endpoint_type_map` and `OutputFormat` from `GenAI-Perf`.
+    These are the full definitions of the endpoints that are supported by AIPerf.
+    Each enum value contains additional metadata about the endpoint, such as whether it supports streaming,
+    produces tokens, and the default endpoint path. This is stored as an attribute on the enum value, and can be accessed
+    via the `info` property. The enum values can still be used as strings for user input and comparison (via the `tag` field).
     """
 
     OPENAI_CHAT_COMPLETIONS = EndpointInfo(
@@ -64,22 +67,27 @@ class EndpointType(CaseInsensitiveStrEnum):
         metrics_title="LLM Metrics",
     )
 
+    # Override the __new__ method to store the Pydantic EndpointInfo model as an attribute. This is a python feature that
+    # allows us to modify the behavior of the enum class's constructor. We use this to ensure the the enums still look like
+    # a regular string enum, but also have the additional information stored as an attribute.
     def __new__(cls, endpoint_info: EndpointInfo) -> Self:
         obj = str.__new__(cls, endpoint_info.tag)
-        # Ensure string value is set for comparison
+        # Ensure string value is set for comparison. This is the how enums work internally.
         obj._value_ = endpoint_info.tag
         # Store the Pydantic model as an attribute
-        obj.info: EndpointInfo = endpoint_info  # type: ignore
+        obj._info: EndpointInfo = endpoint_info  # type: ignore
         return obj
 
     @property
     def info(self) -> EndpointInfo:
         """Get the endpoint info for the endpoint type."""
-        return self.info
+        # This is the Pydantic model that was stored as an attribute in the __new__ method.
+        return self._info  # type: ignore
 
     @property
     def supports_streaming(self) -> bool:
-        """Return True if the endpoint supports streaming. This is used for validation of user input."""  # TODO:: add this validation to the user config
+        """Return True if the endpoint supports streaming. This is used for validation of user input."""
+        # TODO: add this validation to the user config
         return self.info.supports_streaming
 
     @property
@@ -89,10 +97,10 @@ class EndpointType(CaseInsensitiveStrEnum):
 
     @property
     def endpoint_path(self) -> str | None:
-        """Get the default endpoint path for the endpoint type."""
+        """Get the default endpoint path for the endpoint type. If None, the endpoint does not have a specific path."""
         return self.info.endpoint_path
 
     @property
     def metrics_title(self) -> str:
-        """Get the title string for the endpoint type."""
+        """Get the metrics table title string for the endpoint type. If None, the default title is used."""
         return self.info.metrics_title or "LLM Metrics"
