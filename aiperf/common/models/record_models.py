ba904268c88d33e23cce3ded2424e5b644214b1d
Anthony Casagrande <acasagrande@nvidia.com>
2025-10-03 17:56:11 -0700

    more cleranup

--- aiperf/common/models/record_models.py
+++ aiperf/common/models/record_models.py
@@ -12,7 +12,6 @@ from pydantic import (
     SerializeAsAny,
 )
 
-from aiperf.common.constants import NANOS_PER_SECOND
 from aiperf.common.enums import CreditPhase, SSEFieldType
 from aiperf.common.models.base_models import AIPerfBaseModel
 from aiperf.common.models.dataset_models import Turn
@@ -289,77 +288,6 @@ class RequestRecord(AIPerfBaseModel):
             and all(0 < response.perf_ns < sys.maxsize for response in self.responses)
         )
 
-    @property
-    def time_to_first_response_ns(self) -> int | None:
-        """Get the time to the first response in nanoseconds."""
-        if not self.valid:
-            return None
-        return (
-            self.responses[0].perf_ns - self.start_perf_ns
-            if self.start_perf_ns
-            else None
-        )
-
-    @property
-    def time_to_second_response_ns(self) -> int | None:
-        """Get the time to the second response in nanoseconds."""
-        if not self.valid or len(self.responses) < 2:
-            return None
-        return (
-            self.responses[1].perf_ns - self.responses[0].perf_ns
-            if self.responses[1].perf_ns and self.responses[0].perf_ns
-            else None
-        )
-
-    @property
-    def time_to_last_response_ns(self) -> int | None:
-        """Get the time to the last response in nanoseconds."""
-        if not self.valid:
-            return None
-        if self.end_perf_ns is None or self.start_perf_ns is None:
-            return None
-        return self.end_perf_ns - self.start_perf_ns if self.start_perf_ns else None
-
-    @property
-    def inter_token_latency_ns(self) -> float | None:
-        """Get the interval between responses in nanoseconds."""
-        if not self.valid or len(self.responses) < 2:
-            return None
-
-        if (
-            isinstance(self.responses[-1], SSEMessage)
-            and self.responses[-1].packets[-1].value == "[DONE]"
-        ):
-            return (
-                (self.responses[-2].perf_ns - self.responses[0].perf_ns)
-                / (len(self.responses) - 2)
-                if self.responses[-2].perf_ns and self.responses[0].perf_ns
-                else None
-            )
-
-        return (
-            (self.responses[-1].perf_ns - self.responses[0].perf_ns)
-            / (len(self.responses) - 1)
-            if self.responses[-1].perf_ns and self.responses[0].perf_ns
-            else None
-        )
-
-    def token_latency_ns(self, index: int) -> float | None:
-        """Get the latency of a token in nanoseconds."""
-        if not self.valid or len(self.responses) < 1:
-            return None
-        if index == 0:
-            return (
-                self.responses[0].perf_ns - self.recv_start_perf_ns
-                if self.recv_start_perf_ns
-                else None
-            )
-        return (
-            self.responses[index].perf_ns - self.responses[index - 1].perf_ns
-            if self.responses[index].perf_ns and self.responses[index - 1].perf_ns
-            else None
-        )
-
 
 class BaseResponseData(BaseModel):
     """Base class for all response data."""
@@ -474,18 +402,6 @@ class ParsedResponseRecord(AIPerfBaseModel):
             if self.responses
             else sys.maxsize
         )
-
-    @cached_property
-    def request_duration_ns(self) -> int:
-        """Get the duration of the request in nanoseconds."""
-        return self.end_perf_ns - self.start_perf_ns
-
-    @cached_property
-    def tokens_per_second(self) -> float | None:
-        """Get the number of tokens per second of the request."""
-        if self.output_token_count is None or self.request_duration_ns == 0:
-            return None
-        return self.output_token_count / (self.request_duration_ns / NANOS_PER_SECOND)
 
     @cached_property
     def has_error(self) -> bool:
